91124b03acb328843706f344f77cace7a0616231
suhel45 <suhel.ict45@gmail.com>
2022-11-10 00:41:21 +0600

    footer added

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <footer>
-                <small>@copyright</small>
-            </footer>
+        <div className='bg-dark mt-5 '>
+            <section className="">
+  <footer className="text-center text-white">
+    <div className="container p-4 pb-0">
+      <section className="">
+        <p className="d-flex justify-content-center align-items-center">
+          <span className="me-3">Register for free</span>
+          <button type="button" className="btn btn-outline-light btn-rounded">
+            Sign up!
+          </button>
+        </p>
+      </section>
+    </div>
+    <div className="text-center p-3" >
+      © 2020 Copyright
+    </div>
+  </footer>
+</section>
         </div>
     );
 };
