300e4ce474dc3ba6ff57cc4aa73f48210a8a77af
suhel45 <suhel.ict45@gmail.com>
2022-11-12 15:00:48 +0600

    add service

--- src/component/AddService.js
+++ src/component/AddService.js
@@ -1,11 +1,30 @@
-import React from 'react';
+/** @format */
+
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const AddService = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="container">
+      <h3>Add Service</h3>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control type="text" placeholder="Service Name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control type="text" placeholder="Service Price" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control type="text" placeholder="Mobile Number" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default AddService;
